67bef59d2509b621d41cd3b822a80c780af7a4f0
Asland3 <malthe3@outlook.com>
2022-05-05 10:32:12 +0200

    Lidt JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const baseUrl1 = "https://complimentr.com/api";
+const baseUrl2 = "https://insult.mattbas.org/api/insult";
+const baseUrl3 = "https://stressballapi.azurewebsites.net/api/StressBall";
+
 Vue.createApp({
     data() {
+        return {
+            Insult: null,
+            Compliment: null,
+        }
+    },
 
-
-        
+    methods: {
+        async helperGetCompliment() {
+            try {
+                const response = await axios.get(baseUrl1)
+                this.posts = await response.data
+                this.error = null
+            } catch (ex) {
+                alert(ex)
+            }
+        },
     }
 }).mount("#app")
