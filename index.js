5f7fba431418d2dbbe00a24610376bab68debef9
Asland3 <malthe3@outlook.com>
2022-05-10 11:02:22 +0200

    Småting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const baseUrl1 = "https://complimentr.com/api";
 const baseUrl2 = "https://insult.mattbas.org/api/insult";
-const baseUrl3 = "https://stressballapi.azurewebsites.net/api/StressBall";
+const baseUrl3 = "https://newstressballweb.azurewebsites.net/api/StressBall";
 
 Vue.createApp({
     data() {
